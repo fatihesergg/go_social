27c8a9318c8ca88c147e27649276736596bee755
fatihesergg <fatihesergg@gmail.com>
2026-05-10 11:06:32 +0300

    fix: remove secod error wrap

--- internal/services/post.go
+++ internal/services/post.go
@@ -98,7 +98,7 @@ func (ps *PostService) UpdatePost(ctx context.Context, userID uuid.UUID, postIDR
 
 	postID, err := uuid.Parse(postIDRaw)
 	if err != nil {
-		return errors.InvalidIDFormatError.Wrap(fmt.Errorf("Error while parsing postID: %w", err)).Wrap(err)
+		return errors.InvalidIDFormatError.Wrap(fmt.Errorf("Error while parsing postID: %w", err))
 	}
 
 	existPost, err := ps.postStore.GetPostByID(ctx, postID)
